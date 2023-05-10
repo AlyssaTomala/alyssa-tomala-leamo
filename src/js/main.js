568166a63d4fe5513d8fee76c76b478b62ce4747
Alyssa Tomala <alyssa.tomala@studentfr.ch>
2023-05-10 08:33:30 +0200

    07 | footer mobile

--- src/js/main.js
+++ src/js/main.js
@@ -38,3 +38,15 @@ sliders.forEach((slider) => {
     });
   });
 });
+
+function changeBg() {
+  var navbar = document.getElementById("navbar");
+  var scrollValue = window.scrollY;
+  if (scrollValue < 150) {
+    navbar.classList.remove("bgColor");
+  } else {
+    navbar.classList.add("bgColor");
+  }
+}
+
+window.addEventListener("scroll", changeBg);
